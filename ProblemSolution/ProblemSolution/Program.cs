e0edd31c44eb30f90785981fcc59b89cb852ee48
Neha Dipak Gohil <neha-dipak.gohil@capgemini.com>
2023-11-01 15:56:44 +0530

    add sum of two numbers

--- ProblemSolution/ProblemSolution/Program.cs
+++ ProblemSolution/ProblemSolution/Program.cs
@@ -4,8 +4,20 @@
     {
         static void Main(string[] args)
         {
-            HelloWorld helloWorld = new HelloWorld();
-            helloWorld.Print();
+            //1.Write a C# Sharp program to print Hello and your name in a separate line.
+            //HelloWorld helloWorld = new HelloWorld();
+            //helloWorld.Print();
+
+            //2.Write a C# Sharp program to print the sum of two numbers.
+            int num1, num2, result;
+            Console.WriteLine("Please enter number 1 : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter number 2 : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            SumOfTwoNumbers sumOfTwoNumbers = new SumOfTwoNumbers();
+            result = sumOfTwoNumbers.sum(num1 , num2);
+            Console.WriteLine("Sum of two numbers is : " +result);
+
         }
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 | **Sr.No.** | **Question** | **Status** | 
   | -- | -- | -- | 
 | 1 | Write a C# Sharp program to print Hello and your name in a separate line | Completed |
+| 2 | Write a C# Sharp program to print Sum of two numbers | Completed |
