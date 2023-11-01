0107dc38bd16d849529d870cf01f51f4bdb7476a
Neha Dipak Gohil <neha-dipak.gohil@capgemini.com>
2023-11-01 16:18:20 +0530

    add specified operations

--- ProblemSolution/ProblemSolution/Program.cs
+++ ProblemSolution/ProblemSolution/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ProblemSolution
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ProblemSolution
 {
     public class Program
     {
@@ -19,14 +21,26 @@
             //Console.WriteLine("Sum of two numbers is : " +result);
 
             //3. Write a C# Sharp program to print the result of dividing two numbers
-            int num1, num2, result;
-            Console.WriteLine("Please enter number 1 : ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter number 2 : ");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            DivisionOfTwoNumbers divisionOfTwoNumbers = new DivisionOfTwoNumbers();
-            result = divisionOfTwoNumbers.Division(num1 , num2);
-            Console.WriteLine("Division of two numbers is : " + result);
+            //int num1, num2, result;
+            //Console.WriteLine("Please enter number 1 : ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter number 2 : ");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+            //DivisionOfTwoNumbers divisionOfTwoNumbers = new DivisionOfTwoNumbers();
+            //result = divisionOfTwoNumbers.Division(num1 , num2);
+            //Console.WriteLine("Division of two numbers is : " + result);
+
+            //4. Write a C# Sharp program to print the results of the specified operations.
+            //Test data:
+            //-1 + 4 * 6
+            //(35 + 5) % 7
+            //14 + -4 * 6 / 11
+            //2 + 15 / 6 * 1 - 7 % 2
+            //Console.WriteLine(-1 + 4 * 6);
+            //Console.WriteLine((35 + 5) % 7);
+            //Console.WriteLine(14 + -4 * 6 / 11);
+            //Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+
         }
     }
 }
