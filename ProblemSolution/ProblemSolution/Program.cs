e9698814682fdc0383429252e4e3b33c2193ed32
Neha Dipak Gohil <neha-dipak.gohil@capgemini.com>
2023-11-01 16:13:18 +0530

    add division of two numbers

--- ProblemSolution/ProblemSolution/Program.cs
+++ ProblemSolution/ProblemSolution/Program.cs
@@ -4,20 +4,29 @@
     {
         static void Main(string[] args)
         {
-            //1.Write a C# Sharp program to print Hello and your name in a separate line.
+            //1. Write a C# Sharp program to print Hello and your name in a separate line.
             //HelloWorld helloWorld = new HelloWorld();
             //helloWorld.Print();
 
-            //2.Write a C# Sharp program to print the sum of two numbers.
+            //2. Write a C# Sharp program to print the sum of two numbers.
+            //int num1, num2, result;
+            //Console.WriteLine("Please enter number 1 : ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter number 2 : ");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+            //SumOfTwoNumbers sumOfTwoNumbers = new SumOfTwoNumbers();
+            //result = sumOfTwoNumbers.sum(num1 , num2);
+            //Console.WriteLine("Sum of two numbers is : " +result);
+
+            //3. Write a C# Sharp program to print the result of dividing two numbers
             int num1, num2, result;
             Console.WriteLine("Please enter number 1 : ");
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter number 2 : ");
             num2 = Convert.ToInt32(Console.ReadLine());
-            SumOfTwoNumbers sumOfTwoNumbers = new SumOfTwoNumbers();
-            result = sumOfTwoNumbers.sum(num1 , num2);
-            Console.WriteLine("Sum of two numbers is : " +result);
-
+            DivisionOfTwoNumbers divisionOfTwoNumbers = new DivisionOfTwoNumbers();
+            result = divisionOfTwoNumbers.Division(num1 , num2);
+            Console.WriteLine("Division of two numbers is : " + result);
         }
     }
 }
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
   | -- | -- | -- | 
 | 1 | Write a C# Sharp program to print Hello and your name in a separate line | Completed |
 | 2 | Write a C# Sharp program to print Sum of two numbers | Completed |
+| 3 | Write a C# Sharp program to print the result of dividing two numbers | Completed |
