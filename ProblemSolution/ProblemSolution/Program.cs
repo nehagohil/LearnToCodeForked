ee9cf3f9716702a5d2d86a995de02350ae1161fb
Neha Dipak Gohil <neha-dipak.gohil@capgemini.com>
2023-11-02 16:56:03 +0530

    add list of odd numbers 1-99

--- ProblemSolution/ProblemSolution/Program.cs
+++ ProblemSolution/ProblemSolution/Program.cs
@@ -309,7 +309,16 @@ namespace ProblemSolution
             //}
 
             //Console.WriteLine("The longest string is  :"+longest);
-            
+
+            //25.	Write a C# program to print odd numbers from 1 to 99. Prints one number per line.
+
+            for (int i=1; i<=99; i++)
+            {
+                if(i % 2 !=0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
 
         }
     }
