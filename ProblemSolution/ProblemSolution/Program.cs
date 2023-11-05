6e9209ae6eea4a54bf5a6f23cf9fb0f8dc94a005
Neha Dipak Gohil <neha-dipak.gohil@capgemini.com>
2023-11-05 14:34:55 +0530

    add area of shapes

--- ProblemSolution/ProblemSolution/Program.cs
+++ ProblemSolution/ProblemSolution/Program.cs
@@ -1099,46 +1099,94 @@ namespace ProblemSolution
 
             //74.Write a program in C# to read any digit (0-9) and display it in the word using switch case.
 
-            int num;
-            Console.WriteLine("enter number between 0-9:");
-            num = Convert.ToInt32(Console.ReadLine());
+            //int num;
+            //Console.WriteLine("enter number between 0-9:");
+            //num = Convert.ToInt32(Console.ReadLine());
 
-            switch(num)
+            //switch(num)
+            //{
+            //    case 0:
+            //        Console.WriteLine("zero");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine("one");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Two");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Three");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Four");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Five");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Six");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Seven");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("Eight");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Nine");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Please enter number between 0-9");
+            //        break;
+            //}
+
+            //75.Write a C# program which computes the area of various geometrical shapes using a menu-driven approach
+            char ch;
+            do
             {
-                case 0:
-                    Console.WriteLine("zero");
-                    break;
-                case 1:
-                    Console.WriteLine("one");
-                    break;
-                case 2:
-                    Console.WriteLine("Two");
-                    break;
-                case 3:
-                    Console.WriteLine("Three");
-                    break;
-                case 4:
-                    Console.WriteLine("Four");
-                    break;
-                case 5:
-                    Console.WriteLine("Five");
-                    break;
-                case 6:
-                    Console.WriteLine("Six");
-                    break;
-                case 7:
-                    Console.WriteLine("Seven");
-                    break;
-                case 8:
-                    Console.WriteLine("Eight");
-                    break;
-                case 9:
-                    Console.WriteLine("Nine");
-                    break;
-                default:
-                    Console.WriteLine("Please enter number between 0-9");
-                    break;
-            }
+                Console.WriteLine("*************** Welcome to area of various geometrical shapes **************");
+                Console.WriteLine("1. square");
+                Console.WriteLine("2. rectangle");
+                Console.WriteLine("3. circle");
+                Console.WriteLine("0.EXIT");
+                Console.WriteLine("Please enter your choice");
+                ch = Convert.ToChar(Console.ReadLine());
+                switch (ch)
+                {
+                    case '1':
+                        int squareside;
+                        Console.WriteLine("Enter side of the square to get the area");
+                        squareside = Convert.ToInt32(Console.ReadLine());
+                        AreaOfShapes areaOfShapes = new AreaOfShapes();
+                        int areaofsquare = areaOfShapes.getAreaOfSquare(squareside);
+                        Console.WriteLine("area of square is :"+areaofsquare);
+                        break;
+                    case '2':
+                        int length, width;
+                        Console.WriteLine("Enter length of the rectangle");
+                        length = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter width of the rectangle");
+                        width = Convert.ToInt32(Console.ReadLine());
+                        AreaOfShapes areaOfShapes1 = new AreaOfShapes();
+                        int areaofrectangle = areaOfShapes1.getAreaOfRectangle(length,width);
+                        Console.WriteLine("area of square is :" + areaofrectangle);
+                        break;
+                    case '3':
+                        int radius;
+                        Console.WriteLine("Enter radius of a circle");
+                        radius = Convert.ToInt32(Console.ReadLine());
+                        AreaOfShapes areaOfShapes2 = new AreaOfShapes();
+                        double areaofcircle = areaOfShapes2.getAreaOfCircle(radius);
+                        Console.WriteLine("area of circle is :"+ areaofcircle);
+                        break;
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                }
+
+
+            } while (ch != 'y');
+            
         }
 
     }
--- README.md
+++ README.md
@@ -73,8 +73,8 @@
 | 69 | Write a C# program to enter month number and print number of days in month using switch case | complete |
 | 70 | Write a C# program to accept a coordinate point in an XY coordinate system and determine in which quadrant the coordinate point lies | complete |
 | 71 | Write a C# program to determine eligibility for admission to a professional course  | complete |
-| 72 | Write a program in C# to read any digit (0-9) and display it in the word using switch case.  | complete |
-| 73 | Write a program in C# to read any digit (0-9) and display it in the word using switch case.  | complete |
+| 72 | Write a program in C# to read any digit (0-9) and display it in the word using switch case  | complete |
+| 73 | Write a C# program which computes the area of various geometrical shapes using a menu-driven approach  | complete |
 
 
 
